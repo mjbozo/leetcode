dfd22e5a868e60914eacd39f0623b52a8bc3cdc5
Matthew Bourke <matthew.james.bourke@gmail.com>
2024-11-16 14:29:42 +1100

    LC3254: Find the Power of K-Size Subarrays I

---
+++ lc3254.rs
@@ -0,0 +1,71 @@
+// LeetCode problem 3254: Find the Power of K-Size Subarrays I
+// https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/
+
+fn main() {
+    let result = results_array(vec![1, 2, 3, 4, 3, 2, 5], 3);
+    println!("Result = {result:?}");
+}
+
+fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let k = k as usize;
+    let n = nums.len();
+    let s = n - k + 1;
+    let mut results = Vec::with_capacity(s);
+
+    let mut left = 0;
+    let mut right = k - 1;
+    let mut sum = 0;
+
+    for i in 0..k {
+        sum += nums[i];
+    }
+
+    for i in 0..s {
+        let tri = (nums[i + k - 1] * (nums[i + k - 1] + 1)) / 2;
+        let sub = ((nums[i] - 1) * nums[i]) / 2;
+        println!(
+            "i = {i}, tri = {tri}, sub = {sub}, sum = {sum}, nums = {:?}",
+            &nums[i..i + k]
+        );
+
+        if nums[i + k - 1] == nums[i] + k as i32 - 1 && sum == (tri - sub) {
+            println!("Pushing {}", nums[i + k - 1]);
+            results.push(nums[i + k - 1]);
+        } else {
+            println!("Pushing -1");
+            results.push(-1);
+        }
+
+        if right < n - 1 {
+            right += 1;
+            sum += nums[right];
+            sum -= nums[left];
+            left += 1;
+        }
+    }
+
+    return results;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_one() {
+        let result = results_array(vec![1, 2, 3, 4, 3, 2, 5], 3);
+        assert_eq!(result, vec![3, 4, -1, -1, -1]);
+    }
+
+    #[test]
+    fn example_two() {
+        let result = results_array(vec![2, 2, 2, 2, 2], 4);
+        assert_eq!(result, vec![-1, -1]);
+    }
+
+    #[test]
+    fn example_three() {
+        let result = results_array(vec![3, 2, 3, 2, 3, 2], 2);
+        assert_eq!(result, vec![-1, 3, -1, 3, -1]);
+    }
+}
