437dee4e30da08fcbb9e9bd6de380d56a7ee1800
Matthew Bourke <matthew.james.bourke@gmail.com>
2025-02-17 22:26:12 +1100

    LC1718: Construct the Lexicographically Largest Valid Sequence

--- lc1718.rs
+++ lc1718.rs
@@ -1,8 +1,6 @@
 // LeetCode problem 1718: Construct the Lexicographically Largest Valid Sequence
 // https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/description/
 
-use std::collections::HashSet;
-
 fn main() {
     let result = construct_distanced_sequence(5);
     println!("Result = {result:?}");
@@ -10,43 +8,45 @@ fn main() {
 
 fn construct_distanced_sequence(n: i32) -> Vec<i32> {
     let mut res = vec![0; (2 * n - 1) as usize];
-    let mut used = HashSet::new();
+    let mut used = vec![false; (n + 1) as usize];
 
     fill(0, n, &mut res, &mut used);
 
     return res;
 }
 
-fn fill(i: i32, n: i32, mut nums: &mut Vec<i32>, mut used: &mut HashSet<i32>) -> bool {
-    if i == nums.len() as i32 {
+fn fill(current: i32, n: i32, mut nums: &mut Vec<i32>, mut used: &mut Vec<bool>) -> bool {
+    let s = nums.len() as i32;
+    if current == s {
         return true;
     }
 
+    if nums[current as usize] != 0 {
+        return fill(current + 1, n, &mut nums, &mut used);
+    }
+
     for x in (1..=n).rev() {
-        if used.contains(&x) {
+        if used[x as usize] {
             continue;
         }
 
-        if x == 1 && nums[i as usize] == 0 {
-            nums[i as usize] = x;
-            used.insert(x);
+        used[x as usize] = true;
+        nums[current as usize] = x;
 
-            if fill(i + 1, n, &mut nums, &mut used) {
+        if x == 1 {
+            if fill(current + 1, n, &mut nums, &mut used) {
                 return true;
-            } else {
-                used.remove(&x);
             }
-        } else if nums[i as usize] == 0 && nums[(i + x) as usize] == 0 {
-            nums[i as usize] = x;
-            nums[(i + x) as usize] = x;
-            used.insert(x);
-
-            if fill(i + 1, n, &mut nums, &mut used) {
+        } else if current + x < s && nums[(current + x) as usize] == 0 {
+            nums[(current + x) as usize] = x;
+            if fill(current + 1, n, &mut nums, &mut used) {
                 return true;
-            } else {
-                used.remove(&x);
             }
+            nums[(current + x) as usize] = 0;
         }
+
+        nums[current as usize] = 0;
+        used[x as usize] = false;
     }
 
     return false;
